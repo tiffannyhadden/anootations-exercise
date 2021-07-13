5287cdefe9ff9986fa6a1f998375cfc5abec150d
tiffannyhadden <tiffanny.hadden@gmail.com>
2021-07-13 17:45:00 -0500

    adds finish easy_fix

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
 public class Main {
+    @SuppressWarnings("deprecation")
     public static void main(String[] args) {
+
         Author author = new Author("Sandi", "Metz");
 
         author.addBook("Practical Object-Oriented Design in Ruby");
